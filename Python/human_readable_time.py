dd4c23949f1ff4db2fcf73789e2b9d8748852344
gablanouette <30821084+gablanouette@users.noreply.github.com>
2020-06-04 23:59:34 -0400

    Update human_readable_time.py

--- Python/human_readable_time.py
+++ Python/human_readable_time.py
@@ -14,13 +14,18 @@ You can find some examples in the test fixtures.
 '''
 
 
+# first variation
+# def make_readable(seconds):
+#     hr, min, sec = 0, 0, 0
+#     hr = seconds//3600
+#     seconds = seconds % 3600
+#     min = seconds//60
+#     seconds = seconds % 60
+#     sec = seconds
 
-def make_readable(seconds):
-    hr, min, sec = 0, 0, 0
-    hr = seconds//3600
-    seconds = seconds % 3600
-    min = seconds//60
-    seconds = seconds % 60
-    sec = seconds
+#     return '{:02d}'.format(hr) + ':' + '{:02d}'.format(min) + ':' + '{:02d}'.format(sec)
 
-    return '{:02d}'.format(hr) + ':' + '{:02d}'.format(min) + ':' + '{:02d}'.format(sec)
+# second variation; best practice
+def make_readable(s):
+    return '{:02}:{:02}:{:02}'.format(s / 3600, s / 60 % 60, s % 60)
+
